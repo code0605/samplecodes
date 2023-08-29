07c21ef465fa00fb62a18cbf7377a6ccad32a477
code0605 <shrikant.kharade@gmail.com>
2023-08-29 16:43:29 -0400

    Update Jira2.java

--- Jira2.java
+++ Jira2.java
@@ -1,24 +1,54 @@
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class JiraQueryExample {
     public static void main(String[] args) {
-        String jiraUrl = "https://your-jira-instance/rest/api/2/search?jql=your-query-here";
-        String username = "your-username";
-        String apiToken = "your-api-token"; // Or password if using Basic Auth
+        // Jira credentials
+        String username = "your_username";
+        String password = "your_password";
+        
+        // Construct the Jira API URL with the JQL query
+        String jqlQuery = "project=TLC AND Summary ~\"jesd\" AND Status not in (close,open)";
+        String apiUrl = "https://your-jira-instance.com/rest/api/2/search?jql=" + jqlQuery;
+        
+        try {
+            // Create CredentialsProvider and set credentials
+            CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(jiraUrl);
-            request.addHeader("Authorization", "Basic " + Base64.getEncoder()
-                                            .encodeToString((username + ":" + apiToken).getBytes()));
-
-            HttpResponse response = httpClient.execute(request);
-            String jsonResponse = EntityUtils.toString(response.getEntity());
-
-            // Process jsonResponse here
+            // Create HttpClient with basic authentication
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setDefaultCredentialsProvider(credentialsProvider)
+                    .build();
+            
+            // Create HTTP GET request
+            HttpGet httpGet = new HttpGet(apiUrl);
+            
+            // Execute the request
+            HttpResponse response = httpClient.execute(httpGet);
+            
+            // Get the response entity
+            HttpEntity entity = response.getEntity();
+            
+            // Check if response is successful
+            if (response.getStatusLine().getStatusCode() == 200 && entity != null) {
+                // Convert response entity to string
+                String responseBody = EntityUtils.toString(entity);
+                System.out.println(responseBody);
+            } else {
+                System.out.println("Request failed with status code: " + response.getStatusLine().getStatusCode());
+            }
+            
+            // Close the HttpClient
+            httpClient.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
